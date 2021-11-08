8181ce7bb0199a87fac29641db577461cf24de16
Robert <87870041+RobertDec@users.noreply.github.com>
2021-11-08 08:59:31 +0100

    Add files via upload

---
+++ zadanie2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Uczen
+{
+public:
+	string imie, nazwisko, grupa;
+	int rok_urodzenia, klasa;
+
+	
+	void metryczka() {
+		cout << "Imie: Robert";
+		cout << "Nazwisko: Dec";
+		cout << "Klasa: 3Cg";
+	}
+
+};
+Uczen& pobierzDane(Uczen&);
+void pobierzDane(const Uczen&);
+
+int main()
+{
+	Uczen uczen1;
+	pobierzDane(uczen1);
+
+}
+
+Uczen & pobierzDane(Uczen &u) {
+	cout << "Podaj imie:";
+	cin >> u.imie;
+	cout << endl << "Podaj nazwisko:";
+	cin >> u.nazwisko;
+	cout << endl << "Podaj rok urodzenia:";
+	cin >> u.rok_urodzenia;
+	cout << endl << "Podaj grupe:";
+	cin >> u.grupa;
+	cout << endl << "Podaj klase:";
+	cin >> u.klasa;
+
+	cout << endl << "Imie: " << u.imie << endl;
+	cout << "Nazwisko: " << u.nazwisko << endl;
+	cout << "roku_rodzenia: " << u.rok_urodzenia << endl;
+	cout << "Grupa: " << u.grupa << endl;
+	cout << "Klasa: " << u.klasa << endl;
+	return u;
+}
